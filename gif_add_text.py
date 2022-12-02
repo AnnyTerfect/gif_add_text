0701fc37549f7d742a782dd43536f792100f4a97
AnnyTerfect <1247470246@qq.com>
2022-12-02 15:40:55 +0800

    reorganizaed

--- gif_add_text.py
+++ gif_add_text.py
@@ -3,53 +3,59 @@ import imageio
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import sys
+from fire import Fire
 
 class gif_add_text:
-	def choose_font(self, fontpath, fontsize):
+	def __init__(self, filename,
+					   fontpath,
+					   fontsize):
+		gif = imageio.read(filename)
+
+		seq = []
+		seq_len = gif.get_length()
+		for index in range(seq_len):
+			seq.append(gif.get_data(index))
+		self.seq = seq
+		self.duration = gif.get_meta_data()['duration']
 		self.font = ImageFont.truetype(fontpath, fontsize)
-	#字体和字体大小配置
 
-	def load_gif(self, filename):
-		self.gif = imageio.read(filename)
-		self.duration = self.gif.get_meta_data()['duration']
-	#读取gif文件
+	def resize(self, newsize):
+		if newsize:
+			newsize = newsize[1], newsize[0]
+			for index, img in enumerate(self.seq):
+				h, w, c = img.shape
+				new_img = 255 + np.zeros(newsize + (c,), dtype='uint8')
+				new_img[: h, :w, :c] = img
+				self.seq[index] = new_img
 
-	def add_text(self, text, pos, color):
-		self.seq = []
-		l = self.gif.get_length()
-		for i in range(l):
-			self.seq.append(self.gif.get_data(i))
-			#从gif中获取位图序列
-
-		for i in range(l):
+	def add_text(self, text, pos, color, time_start=0):
+		seq_len = len(self.seq)
+		for i in range(time_start, seq_len):
 			img = Image.fromarray(self.seq[i])
 			draw = ImageDraw.Draw(img)
-			draw.text(pos, unicode(text, 'utf-8'), font = self.font, fill = color)
+			draw.text(pos, text, font=self.font, fill=color)
 			#写入文字
 			self.seq[i] = np.asarray(img)
 			#转成np数组
 
 	def save(self, filename):
-		imageio.mimsave(filename, self.seq, duration = self.duration / 1000.0)
-		#保存图片
+		imageio.mimsave(filename, self.seq, duration=self.duration / 1000.0)
+
+def main(
+	gif='sorry.gif',
+	new_gif='new_sorry.gif',
+	text='哈哈',
+	font='微软雅黑.ttf',
+	newsize=None,
+	font_size=40,
+	pos=(90, 20),
+	color=(255, 255, 255),
+	time_start=0
+):
+	app = gif_add_text(gif, font, font_size)
+	app.resize(newsize)
+	app.add_text(text, pos, color, time_start)
+	app.save(new_gif)
 
 if __name__ == '__main__':
-	if len(sys.argv) < 8:
-		print('please input args follow the format bellow')
-		print('python gif_add_text.py [fontpath] [fontsize] [gifpath] [text] [x,y] [r,g,b] [new_gif_path]')
-		exit()
-	font = sys.argv[1]
-	fontsize = int(sys.argv[2])
-	gif = sys.argv[3]
-	text = sys.argv[4]
-	x, y = sys.argv[5].replace('(', '').replace(')', '').split(',')
-	x, y = int(x), int(y)
-	r, g, b = sys.argv[6].replace('(', '').replace(')', '').split(',')
-	r, g, b = int(r), int(g), int(b)
-	new_gif = sys.argv[7]
-
-	app = gif_add_text()
-	app.choose_font(font, fontsize)
-	app.load_gif(gif)
-	app.add_text(text, (x, y), (r, g, b))
-	app.save(new_gif)
+	Fire(main)
